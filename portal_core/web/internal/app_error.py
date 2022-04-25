96d79c5458903bcfa35f6219c1a67678893740c4
Max von Tettenborn <max@vtettenborn.net>
2022-04-25 15:37:52 +0200

    test host header for error page

--- portal_core/web/internal/app_error.py
+++ portal_core/web/internal/app_error.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 log = logging.getLogger(__name__)
 
@@ -8,5 +8,5 @@ router = APIRouter()
 
 
 @router.get('/app_error/{status}')
-def app_error(status):
-	return f'Splash for error {status}'
+def app_error(status, request: Request):
+	return f'Splash for error {status} and host {request.headers.get("host")}'
