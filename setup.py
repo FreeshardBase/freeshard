5de36dac4495dda1495f4f84b06abe5b511e8480
Max von Tettenborn <max@vtettenborn.net>
2022-12-29 14:20:58 +0100

    set version to 0.17.0.dev0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ log = logging.getLogger(__name__)
 
 setup(
 	name='portal_core',
-	version='0.16.0',
+	version='0.17.0.dev0',
 	packages=find_packages(),
 	url='https://gitlab.com/ptl/portal_core',
 	author='Max von Tettenborn',
