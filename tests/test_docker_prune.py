7a774f3f09b96f31b121c82cdd8a3e34de7b5760
Max von Tettenborn <max@vtettenborn.net>
2023-09-19 21:47:55 +0200

    fix tests

--- tests/test_docker_prune.py
+++ tests/test_docker_prune.py
@@ -8,16 +8,16 @@ function_config_override = {'apps': {'pruning': {'schedule': '* * * * * *'}}}
 
 @pytest.mark.config_override(function_config_override)
 async def test_docker_prune(api_client: AsyncClient, memory_logger):
-	await asyncio.sleep(2)
+	await asyncio.sleep(1)
 	assert any(['docker images pruned' in r.msg for r in memory_logger.records])
 
 
 async def test_not_docker_prune(api_client: AsyncClient, memory_logger):
-	await asyncio.sleep(2)
+	await asyncio.sleep(1)
 	assert not any(['docker images pruned' in r.msg for r in memory_logger.records])
 
 
 async def test_docker_prune_manually(api_client: AsyncClient, memory_logger):
 	response = await api_client.post('/protected/settings/prune-images')
-	assert response.status_code == 204
+	assert response.status_code == 200
 	assert any(['docker images pruned' in r.msg for r in memory_logger.records])
