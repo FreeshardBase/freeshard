a22ee74f0e095f0327e18df88c7d49057ac7fafe
Max von Tettenborn <max@vtettenborn.net>
2024-01-06 19:43:43 +0100

    add pretty_name field for apps

--- portal_core/model/app_meta_migration.py
+++ portal_core/model/app_meta_migration.py
@@ -1,3 +1,9 @@
+def migrate_1_0_to_1_1(values):
+	values['v'] = '1.1'
+	values['pretty_name'] = values['name'].title()
+	return values
+
+
 migrations = {
-	# '1.0': migrate_1_0_to_X_X,
+	'1.0': migrate_1_0_to_1_1,
 }
---
+++ tests/test_app_meta_migration.py
@@ -0,0 +1,22 @@
+from portal_core.model.app_meta import AppMeta, Lifecycle, PortalSize
+
+
+def test_migrate_1_0_to_1_1():
+	app_meta_in = AppMeta(
+		v='1.0',
+		app_version='1.0.0',
+		name='test',
+		pretty_name='not set',
+		icon='test',
+		entrypoints=[],
+		paths={},
+		lifecycle=Lifecycle(always_on=False, idle_time_for_shutdown=60),
+		minimum_portal_size=PortalSize.XS,
+		store_info=None,
+	)
+	app_meta_in_json = app_meta_in.dict(exclude={'pretty_name'})
+	app_meta_in_json['v'] = '1.0'
+
+	app_meta_out = AppMeta.validate(app_meta_in_json)
+	assert app_meta_out.v == '1.1'
+	assert app_meta_out.pretty_name == 'Test'
