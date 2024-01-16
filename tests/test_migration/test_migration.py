bbebd6380a51ec8f199f6410a2ebd3d8752918bb
Max von Tettenborn <max@vtettenborn.net>
2024-01-16 10:07:45 +0100

    reproduce problem with incomplete app migration as test

--- tests/test_migration/test_migration.py
+++ tests/test_migration/test_migration.py
@@ -42,7 +42,8 @@ def init_db_file_nonexisting_app(tmp_path):
 	shutil.copy(init_db_file, dest)
 
 
-async def test_migration_nonexisting_app(init_db_file_nonexisting_app, api_client, tmp_path):
+async def test_migration_nonexisting_app(
+		init_db_file_nonexisting_app, api_client, tmp_path):
 	response = (await api_client.get('protected/apps')).json()
 	assert len(response) == 1
 	assert any(app['name'] == 'filebrowser' for app in response)
@@ -55,3 +56,21 @@ async def test_migration_nonexisting_app(init_db_file_nonexisting_app, api_clien
 	with open(tmp_path / 'path_root' / 'core' / 'portal_core_db.json') as f:
 		db = json.load(f)
 	assert 'apps' not in db
+
+
+@pytest.fixture
+def init_db_file_incomplete_migration(tmp_path):
+	init_db_file = Path(__file__).parent / 'init_db_incomplete_migration.json'
+	dest = tmp_path / 'path_root' / 'core' / 'portal_core_db.json'
+	dest.parent.mkdir(parents=True, exist_ok=True)
+	dest.touch()
+	shutil.copy(init_db_file, dest)
+
+
+async def test_migration_incomplete_migration(
+		init_db_file_incomplete_migration, api_client, tmp_path):
+	with open(tmp_path / 'path_root' / 'core' / 'portal_core_db.json') as f:
+		db = json.load(f)
+	assert 'apps' not in db
+	assert 'installed_apps' in db
+	assert db['installed_apps']['1']['name'] == 'filebrowser'
