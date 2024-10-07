550c0fc07dd8864cc7fd3a34e2d5f939760f2e7c
Max von Tettenborn <max@vtettenborn.net>
2024-10-07 09:56:40 +0200

    release 0.30.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ log = logging.getLogger(__name__)
 
 setup(
 	name='portal_core',
-	version='0.31.0.dev0',
+	version='0.30.1',
 	packages=find_packages(),
 	url='https://gitlab.com/ptl/portal_core',
 	author='Max von Tettenborn',
