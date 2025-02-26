70e0acba2ceccebe38b376cc3856b771210a9743
Max von Tettenborn <max@vtettenborn.net>
2025-02-26 13:30:49 +0100

    add deploy tools

---
+++ deploy_tools/bootstrap.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/FreeshardBase/freeshard/refs/heads/main/deploy_tools/docker-compose.yml
+sudo touch acme.json traefik.yml
+sudo chmod 600 acme.json
---
+++ deploy_tools/reset.sh
@@ -0,0 +1,1 @@
+sudo rm -r acme.json installed_apps shard_core_db.json traefik.yml traefik_dyn user_data
